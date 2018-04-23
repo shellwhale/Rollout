855b468860fe17ef643dc8df3423257cc8178e36
ShellWhale <itshellwhale@protonmail.ch>
2018-04-23 11:23:19 +0200

    Passage en ISO/CEI 8859-1

--- rollout.ps1
+++ rollout.ps1
@@ -1,29 +1,42 @@
-# RÃ©cupÃ©ration de l'architecture choisie
+# Récupération de l'architecture choisie
 param([string]$arch)
-# Titre de la fenÃªtre
+
+
+
+# Le script doit être lancé depuis le dossier Rollout
+if (-Not ((Get-Item -Path ".\").name -contains 'Rollout' ) ) 
+{
+    Write-Host "Ce script doit être lancé depuis le répertoire Rollout"
+    exit    
+}
+
+# Titre de la fenêtre
 $host.ui.RawUI.WindowTitle = "Rollout"
 
-# Si elle est autre que amd64 ou x86, arrÃªt du script
+
+
+
+# Si elle est autre que amd64 ou x86, arrêt du script
 If (-Not ($arch -contains 'amd64' -Or $arch -contains 'x86'))
 {
     Write-Host ".\rollout.ps1 <amd64/x86>"
     exit
 }
-# Titre de la fenÃªtre
+# Titre de la fenêtre
 $host.ui.RawUI.WindowTitle = "Rollout $arch"
 
-# Installation de BurntToast si il n'est pas dÃ©jÃ  prÃ©sent 
+# Installation de BurntToast si il n'est pas déjà présent 
 if (-Not(Test-Path C:\Users\$env:USERNAME\Documents\WindowsPowerShell\modules\BurntToast)){Copy-Item ./Modules/BurntToast C:\Users\$env:USERNAME\Documents\WindowsPowerShell\modules\BurntToast}
 
-# DÃ©ploiement d'un WinPE via ADK Ã  la racine selon l'architecture choisie
+# Déploiement d'un WinPE via ADK à la racine selon l'architecture choisie
 Get-Content "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\DandISetEnv.bat" | Out-File "$env:temp\DandISetEnv$arch.bat" -encoding ASCII
 Add-Content -Path $env:temp\DandISetEnv$arch.bat -Value "copype $arch C:\$arch"
 cmd.exe /c $env:temp\DandISetEnv$arch.bat
 
-# Montage de l'image Ã  la racine selon l'architecture choisie 
+# Montage de l'image à la racine selon l'architecture choisie 
 Dism /Mount-Image /ImageFile:"C:\$arch\media\sources\boot.wim" /Index:1 /MountDir:"C:\$arch\mount"
 
-# Configuration de la rÃ©gion sur fr-BE
+# Configuration de la région sur fr-BE
 Dism /image:c:\$arch\mount /Set-SysLocale:fr-BE
 Dism /image:c:\$arch\mount /Set-UserLocale:fr-BE
 Dism /image:c:\$arch\mount /Set-InputLocale:fr-BE
@@ -52,10 +65,10 @@ Dism /Add-Package /Image:"C:\$arch\mount" /PackagePath:"C:\Program Files (x86)\W
 Dism /Add-Package /Image:"C:\$arch\mount" /PackagePath:"C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\$arch\WinPE_OCs\WinPE-SecureBootCmdlets.cab"
 Dism /Add-Package /Image:"C:\$arch\mount" /PackagePath:"C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Windows Preinstallation Environment\$arch\WinPE_OCs\en-us\WinPE-SecureBootCmdlets_en-us.cab"
 
-# DÃ©montage et sauvegarde de l'image
+# Démontage et sauvegarde de l'image
 Dism /Unmount-Image /MountDir:C:\$arch\mount /Commit
 
-# DÃ©placement du WinPE vers le dossier Winbox
+# Déplacement du WinPE vers le dossier Winbox
 Move-Item C:\$arch .\Winbox\
 $logo = "$PWD\Ressources\logo.jpg"
-New-BurntToastNotification -Text "Rollout", "Une Winbox $arch est prÃªte  Terrible !" -AppLogo $logo
+New-BurntToastNotification -Text "Rollout", "Une Winbox $arch est prête  Terrible !" -AppLogo $logo
